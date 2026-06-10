7252b0f356fe4a9556e4b78a61c4f8a1eab00b32
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-06-10 16:32:08 +0700

    feat: add login validation and authentication flow

--- src/js/auth/login.js
+++ src/js/auth/login.js
@@ -1,18 +1,28 @@
-const form = document.querySelector("form")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
-form.addEventListener("submit" , (e)=>{
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if(
-        email.value == "" || password.value == ""
-    ) {
-        alert("Semua field wajib diisi")
-        return
-    }
+  if (email.value == "" || password.value == "") {
+    alert("Semua field wajib diisi");
+    return;
+  }
 
-     console.log(email.value)
-    console.log(password.value)
+  const user = JSON.parse(localStorage.getItem("user"));
 
-})
+  if (!user) {
+    alert("Akun anda belum terdaftar");
+    return;
+  }
+
+  if (email.value === user.email && password.value === user.password) {
+    alert("Login Berhasil!!!");
+    window.location.href = "../page/index.html";
+    return;
+  } else {
+    alert("Email atau Password salah!!");
+  }
+  consolelog(user)
+});
